616bd0abd1d099a88c7fe1feb4bca862e4a0efc7
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-15 14:16:40 -0400

    adding public folders for easier navigation as we build higher

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express()
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+
+app.use(express.static(path.join(__dirname, "public")))
+
 const PORT = process.env.PORT || 3000
 
 app.get("/", function(req, res){
