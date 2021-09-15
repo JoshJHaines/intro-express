333b1dc913c0797fe73637d2dde01fe58e11ed3a
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-15 12:47:19 -0400

    console logging path

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require("path")
 
 const app = express()
 
+console.log(__dirname)
+console.log(path.join(__dirname, "views"))
+
 app.set("views", path.join(__dirname, "views"))
 
 const PORT = process.env.PORT || 3000
