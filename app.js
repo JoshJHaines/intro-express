71515ad1ca17f712e9697ce543c2d9f234020adb
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-15 12:42:37 -0400

    added path

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
+
+app.set("views", path.join(__dirname, "views"))
 
 const PORT = process.env.PORT || 3000
 
